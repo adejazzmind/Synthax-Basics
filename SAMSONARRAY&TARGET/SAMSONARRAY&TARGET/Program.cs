3c26041cd8605f62b097b3fdba80e95ec94a2b84
adejazzmind <adejazzmind@gmail.com>
2025-05-24 08:41:14 -0700

    initial commit

---
+++ SAMSONARRAY&TARGET/SAMSONARRAY&TARGET/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY_TARGET
+{
+    public class Program
+    {
+        //track 1
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 4, 2, 7 };
+            int target = 9;
+
+            AddNumber(nums, target);
+        }
+        public static void AddNumber(int[] nums, int target)
+        {
+            //using loop
+            bool resultFound = false;
+            for (int i = 0; i < nums.Length; i++)
+            {
+               for (int j = i + 1; j < nums.Length; j++)
+               { 
+                    if (nums[i] + nums[j] == target)
+                    {
+                        resultFound = true;
+                        Console.WriteLine("The indices are: are [{0}], [{1}]", i, j);
+                        Console.WriteLine($"The indices are: [{i}],[{j}]");
+                    }
+                }
+            }
+            if (resultFound == false)
+                Console.WriteLine("Could not find solution!");
+        }
+    }
+}
